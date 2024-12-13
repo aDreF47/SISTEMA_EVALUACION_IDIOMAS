90e5143c94b364ff32185a21a1047f964d85a8f7
Fernando <cgfernando.4799@gmail.com>
2024-12-13 02:17:27 -0500

    dando mejoras en diseño y bonotes en las dos primeras tab de cliente

--- src/controllers/ClienteController.java
+++ src/controllers/ClienteController.java
@@ -1,13 +1,18 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.Optional;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -25,12 +30,6 @@ import javafx.stage.Stage;
  */
 public class ClienteController {
 
-    @FXML
-    private Label idnombre;
-    @FXML
-    private Label docentecodigo;
-    @FXML
-    private Label docentecorreo;
     @FXML
     private TableView<?> tablaReporte;
     @FXML
@@ -69,28 +68,69 @@ public class ClienteController {
     private Tab tabMisCursos;
     @FXML
     private TabPane paneCliente;
+    @FXML
+    private DatePicker datePickerPago;
+    @FXML
+    private Button btnAceptarPago;
+    @FXML
+    private Label lblIdioma;
+    @FXML
+    private Label lblHoraInicio;
+    @FXML
+    private Label lblHoraFin;
+    @FXML
+    private Label lblDia;
+    @FXML
+    private Label lblDocente;
+    @FXML
+    private Label lblVacantes;
+    @FXML
+    private Button btnAdquirirCurso;
+    @FXML
+    private TableColumn<?, ?> colFechaIni;
+    @FXML
+    private TableColumn<?, ?> colFechFin;
+    @FXML
+    private Label lblNombre;
+    @FXML
+    private Label lblCodigoEstudiante;
+    @FXML
+    private Label lblEmail;
+    @FXML
+    private Label lblApellido;
     
-    @FXML
-    private void HorarioAction(ActionEvent event) {
-    }
-
-    @FXML
-    private void ReporteAction(ActionEvent event) {
-    }
 
     @FXML
     private void btnSalirAction(ActionEvent event) throws IOException {
-        Parent regresarLogin = FXMLLoader.load(getClass().getResource("/views/LoginView.fxml"));
-        Scene registrarScene = new Scene(regresarLogin);
+        Optional<ButtonType> result = mostrarMensajeAlerta(Alert.AlertType.INFORMATION, "Confirmacion", "Seguro que desea salir?");
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            System.out.println("Saliendo..."); // Actualizar la tabla después de eliminar
+            //logica para salir
+            //limpiarCampos();
+
+            Parent regresarLogin = FXMLLoader.load(getClass().getResource("/views/LoginView.fxml"));
+            Scene registrarScene = new Scene(regresarLogin);
+            
+            registrarScene.getStylesheets().add(getClass().getResource("/styles/styleLogin.css").toExternalForm());
+            // Obtener el escenario actual y cambiar la escena
+            Stage window = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            
+            window.setScene(registrarScene);
+            window.centerOnScreen();
+            window.show();
+        }
+
+
         
-        registrarScene.getStylesheets().add(getClass().getResource("/styles/styleLogin.css").toExternalForm());
-        // Obtener el escenario actual y cambiar la escena
-        Stage window = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-        
-        window.setScene(registrarScene);
-        window.centerOnScreen();
-        window.show();
     
+    }
+
+    private Optional<ButtonType> mostrarMensajeAlerta(Alert.AlertType tipo, String titulo, String message) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        return alert.showAndWait();
     }
 
     @FXML
@@ -161,8 +201,19 @@ public class ClienteController {
     }
 
     @FXML
-    private void DisponibilidadAction(ActionEvent event) {
+    private void irPanelHorarioAction(ActionEvent event) {
+        paneCliente.getSelectionModel().select(tabModulos);
+    }
+
+    @FXML
+    private void irPanelMisCursosAction(ActionEvent event) {
+        paneCliente.getSelectionModel().select(tabMisCursos);
+    }
+
+    @FXML
+    private void btnRegresarPanelHorarioAction(ActionEvent event) {
+        paneCliente.getSelectionModel().select(tabModulos);
+
     }
  
-    
 }
